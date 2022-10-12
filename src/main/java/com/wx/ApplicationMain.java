5c59302a545411efc9903743cb00cdcfef5fa228
vincentenxy <vincentenxy@outlook.com>
2022-10-12 23:57:41 +0800

    删除文件

--- src/main/java/com/wx/ApplicationMain.java
+++ src/main/java/com/wx/ApplicationMain.java
@@ -3,6 +3,7 @@ package com.wx;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 /**
  * Project: stu
@@ -12,6 +13,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 @MapperScan("com.wx.security")
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class ApplicationMain {
 
     public static void main(String[] args) {
